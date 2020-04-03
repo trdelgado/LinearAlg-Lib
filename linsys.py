09a90d70fa9c23aff02003bb41d0c6e2c2310c61
trdelgado <trdelgado19@gmail.com>
2020-04-02 19:48:01 -0700

    Add rref to reduce system of equations to rref form.

--- linsys.py
+++ linsys.py
@@ -108,6 +108,43 @@ class LinearSystem(object):
         return system
 
 
+    def compute_rref(self):
+        tf = self.compute_triangular_form()
+        num_equations = len(tf)
+        num_variables = tf.dimension
+
+        # From last row to first row
+        for i in range(num_equations-1, -1, -1):
+
+            # Get leading coefficient
+            j = -1
+            p = tf.planes[i].normal_vector.coordinates
+
+            for k, item in enumerate(p):
+                if not MyDecimal(item).is_near_zero():
+                    j = k
+                    break
+
+            if j != -1:
+
+                # Find beta: reciprocal of coefficient
+                ceof = tf[i].normal_vector.coordinates[j]
+                recp_ceof = MyDecimal(1/ceof)
+
+                # Multiply row by coefficient
+                tf.multiply_coefficient_and_row(recp_ceof, i)
+
+                # For each row above current row
+                for k in range(i-1, -1, -1):
+
+                    # Find the reciprocal of the coefficient i of current row
+                    alpha = -tf[k].normal_vector.coordinates[j]
+
+                    # Add negative multiply reciprocal to current row
+                    tf.add_multiple_times_row_to_row(alpha, i, k)
+
+        return tf
+
     def indices_of_first_nonzero_terms_in_each_row(self):
         num_equations = len(self)
         num_variables = self.dimension
@@ -215,6 +252,7 @@ def test_set1():
             s[3] == p3):
         print('test case 9 failed')
 
+
 def test_set2():
     p1 = Plane(normal_vector=Vector(['1','1','1']), constant_term='1')
     p2 = Plane(normal_vector=Vector(['0','1','1']), constant_term='2')
@@ -253,4 +291,44 @@ def test_set2():
     if not (t[0] == Plane(normal_vector=Vector(['1','-1','1']), constant_term='2') and
             t[1] == Plane(normal_vector=Vector(['0','1','1']), constant_term='1') and
             t[2] == Plane(normal_vector=Vector(['0','0','-9']), constant_term='-2')):
+        print('test case 4 failed')
+
+
+def test_set3():
+    p1 = Plane(normal_vector=Vector(['1','1','1']), constant_term='1')
+    p2 = Plane(normal_vector=Vector(['0','1','1']), constant_term='2')
+    s = LinearSystem([p1,p2])
+    r = s.compute_rref()
+    if not (r[0] == Plane(normal_vector=Vector(['1','0','0']), constant_term='-1') and
+            r[1] == p2):
+        print('test case 1 failed')
+
+    p1 = Plane(normal_vector=Vector(['1','1','1']), constant_term='1')
+    p2 = Plane(normal_vector=Vector(['1','1','1']), constant_term='2')
+    s = LinearSystem([p1,p2])
+    r = s.compute_rref()
+    if not (r[0] == p1 and
+            r[1] == Plane(constant_term='1')):
+        print('test case 2 failed')
+
+    p1 = Plane(normal_vector=Vector(['1','1','1']), constant_term='1')
+    p2 = Plane(normal_vector=Vector(['0','1','0']), constant_term='2')
+    p3 = Plane(normal_vector=Vector(['1','1','-1']), constant_term='3')
+    p4 = Plane(normal_vector=Vector(['1','0','-2']), constant_term='2')
+    s = LinearSystem([p1,p2,p3,p4])
+    r = s.compute_rref()
+    if not (r[0] == Plane(normal_vector=Vector(['1','0','0']), constant_term='0') and
+            r[1] == p2 and
+            r[2] == Plane(normal_vector=Vector(['0','0','-2']), constant_term='2') and
+            r[3] == Plane()):
+        print('test case 3 failed')
+
+    p1 = Plane(normal_vector=Vector(['0','1','1']), constant_term='1')
+    p2 = Plane(normal_vector=Vector(['1','-1','1']), constant_term='2')
+    p3 = Plane(normal_vector=Vector(['1','2','-5']), constant_term='3')
+    s = LinearSystem([p1,p2,p3])
+    r = s.compute_rref()
+    if not (r[0] == Plane(normal_vector=Vector(['1','0','0']), constant_term=Decimal('23')/Decimal('9')) and
+            r[1] == Plane(normal_vector=Vector(['0','1','0']), constant_term=Decimal('7')/Decimal('9')) and
+            r[2] == Plane(normal_vector=Vector(['0','0','1']), constant_term=Decimal('2')/Decimal('9'))):
         print('test case 4 failed')
